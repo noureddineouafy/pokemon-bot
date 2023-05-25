ad2d58381c86ef07f58b04575bf14f10cc380d7b
noureddineouafy <130229691+noureddineouafy@users.noreply.github.com>
2023-05-25 15:32:48 +0100

    Update youtube-as-mp3.js

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -1,8 +1,8 @@
 import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args }) => {
-if (!args[0]) throw 'هذا الامر خاص بتحميل للفيديوات '
-await m.reply( 'جاري تحميل الفيذيو المرجو الانتظار قليلا رجاء في حالة لم يقم البوت بتحميل الفيديو الخاص بك  اعلم ان الفيديو طويل او ان حجمه كبير  ويمكنك مراسلة نورالدين للمزيد من المعلومات instagram.com/noureddine_ouafy')
+if (!args[0]) throw 'هذا الامر خاص بتحميل الفيديوات على شكل mp3 من اليوتوب '
+await m.reply( 'جاري تحميل mp3 المرجو الانتظار قليلا رجاء في حالة لم يقم البوت بتحميل mp3 الخاص بك  اعلم ان الفيديو طويل او ان حجمه كبير  ويمكنك مراسلة نورالدين للمزيد من المعلومات instagram.com/noureddine_ouafy')
 try {
 let q = '128kbps'
 let v = args[0]
